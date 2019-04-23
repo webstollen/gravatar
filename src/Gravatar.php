313814977e2245bd7250c03133ff4aa49fe659a2
Christian <proske@webstollen.de>
2019-04-23 15:22:59 +0200

    Update Gravatar.php

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -19,22 +19,29 @@ class Gravatar
         $this->http = $http;
     }
 
-    public function exists(string $email): bool
+    public function exists(string $email, bool $hased = false): bool
     {
         try {
+            if($hashed){
+                return $this->http->head(sprintf(self::PROFILE_URL, $email)->getStatusCode() === 200;
+            }
             return $this->http->head(sprintf(self::PROFILE_URL, $this->emailHash($email)))->getStatusCode() === 200;
         } catch(RequestException $e) {
             return false;
         }
     }
 
-    public function imageUrl(string $email, int $size = null): string
+    public function imageUrl(string $email, int $size = null, bool $hased = false): string
     {
         $_size = '';
         if (!is_null($size)) {
             $_size = '?size=' . $size;
         }
 
+        if($hashed){
+            return sprintf(self::IMAGE_URL, $email, $_size);
+        }
+                                         
         return sprintf(self::IMAGE_URL, $this->emailHash($email), $_size);
     }
 
